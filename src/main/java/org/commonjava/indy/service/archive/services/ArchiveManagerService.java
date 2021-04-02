368c953898bb9dde2f787c2b3ee7568839d2acdb
liyu <liyu@redhat.com>
2021-04-02 18:45:35 +0800

    refactor error sending step

--- src/main/java/org/commonjava/indy/service/archive/services/ArchiveManagerService.java
+++ src/main/java/org/commonjava/indy/service/archive/services/ArchiveManagerService.java
@@ -58,91 +58,77 @@ public class ArchiveManagerService {
     ArchiveController controller;
 
     public Uni<Response> create(final UriInfo uriInfo,final HttpRequest request) {
+
         HistoricalContentDTO content;
-        try
-        {
+
+        try {
             String json = IOUtils.toString( request.getInputStream(), Charset.defaultCharset() );
             content = objectMapper.readValue( json, HistoricalContentDTO.class );
-            if ( content == null )
-            {
-                return responseHelper.fromResponseReactive("Failed to read historical content which is empty.")
-                        .onItem().transform(message -> Response.status(500,message))
-                        .onItem().transform(Response.ResponseBuilder::build);
+            if ( content == null ) {
+                return sendError(500,"Failed to read historical content which is empty.");
             }
-        }
-        catch ( final IOException e )
-        {
-            return responseHelper.fromResponseReactive("Failed to read historical content file from request body.")
-                    .onItem().transform(message -> Response.status(500,message))
-                    .onItem().transform(Response.ResponseBuilder::build);
+        } catch ( final IOException e ) {
+            return sendError(500,"Failed to read historical content file from request body.");
         }
 
-        try
-        {
+        try {
             Map<String, String> downloadPaths = reader.readPaths( content );
             controller.downloadArtifacts( downloadPaths, content );
             Optional<File> archive = controller.generateArchive( content );
             if ( archive.isEmpty() )
             {
-                return responseHelper.fromResponseReactive("Failed to get downloaded contents for archive.")
-                        .onItem().transform(message -> Response.status(500,message))
-                        .onItem().transform(Response.ResponseBuilder::build);
+                return sendError(500,"Failed to get downloaded contents for archive.");
             }
             controller.renderArchive( archive.get(), content.getBuildConfigId() );
+        } catch ( InterruptedException e ) {
+            return sendError(500,"Artifacts downloading is interrupted.");
+        } catch ( final ExecutionException e ) {
+            return sendError(500,"Artifacts download execution manager failed.");
+        } catch ( final IOException e ) {
+
+            return sendError(500,"Failed to generate historical archive from content.");
         }
-        catch ( InterruptedException e )
-        {
-            return responseHelper.fromResponseReactive("Artifacts downloading is interrupted.")
-                    .onItem().transform(message -> Response.status(500,message))
-                    .onItem().transform(Response.ResponseBuilder::build);
-        }
-        catch ( final ExecutionException e )
-        {
-            return responseHelper.fromResponseReactive("Artifacts download execution manager failed.")
-                    .onItem().transform(message -> Response.status(500,message))
-                    .onItem().transform(Response.ResponseBuilder::build);
-        }
-        catch ( final IOException e )
-        {
-            return responseHelper.fromResponseReactive("Failed to generate historical archive from content.")
-                    .onItem().transform(message -> Response.status(500,message))
-                    .onItem().transform(Response.ResponseBuilder::build);
-        }
+
         return Uni.createFrom().item(uriInfo.getRequestUri())
                 .onItem().transform(uri -> Response.created(uri).build());
     }
 
     public Uni<Boolean> delete(final String buildConfigId){
-        try
-        {
+
+        try {
             controller.deleteArchive( buildConfigId );
-        }
-        catch ( final IOException e )
-        {
+        } catch ( final IOException e ) {
             return Uni.createFrom().item(false);
         }
+
         return Uni.createFrom().item(true);
+
     }
 
     public Uni<Response> get( String buildConfigId, final UriInfo uriInfo ){
         InputStream inputStream = null;
+
         try {
             Optional<File> file = controller.getArchiveInputStream( buildConfigId );
             if(file.isPresent()){
                 inputStream = FileUtils.openInputStream( file.get() );
             }
         } catch ( final IOException e ) {
-            return responseHelper.fromResponseReactive("Failed to generate historical archive from content.")
-                    .onItem().transform(message -> Response.status(500,message))
-                    .onItem().transform(Response.ResponseBuilder::build);
+            return sendError(500,"Failed to generate historical archive from content.");
         }
+
         return buildWithHeader(Response.ok(new TransferStreamingOutput( inputStream )),buildConfigId);
     }
 
-    private Uni<Response> buildWithHeader(Response.ResponseBuilder builder, final String buildConfigId )
-    {
+    private Uni<Response> buildWithHeader(Response.ResponseBuilder builder, final String buildConfigId ) {
         return Uni.createFrom().item(new StringBuilder())
                 .onItem().transform(header -> header.append("attachment;").append( "filename=" ).append( buildConfigId ).append( ".zip" ))
                 .onItem().transform(re -> builder.header("Content-Disposition", re.toString()).build());
+    }
+
+    private Uni<Response> sendError(int status, String message){
+        return responseHelper.fromResponseReactive(message)
+                .onItem().transform(re -> Response.status(status,re))
+                .onItem().transform(Response.ResponseBuilder::build);
     }
 }
